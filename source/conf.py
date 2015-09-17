5110f4b2643494ad8a9c00090c928ea4b365e3a8
Will <will.doran@riseup.net>
2015-09-17 18:45:58 -0400

    adding requirements and changing to direct to ushahidi custom theme

--- source/conf.py
+++ source/conf.py
@@ -119,13 +119,13 @@ todo_include_todos = True
 # html_theme = 'alabaster'
 
 # on_rtd is whether we are on readthedocs.org
-import os
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+#import os
+#on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
-if not on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+#if not on_rtd:  # only import and set the theme if we're building docs locally
+#    import sphinx_rtd_theme
+html_theme = 'ushahidi'
+html_theme_path = ['_theme']
 # otherwise, readthedocs.org uses their theme by default, so no need to specify it
 
 # Theme options are theme-specific and customize the look and feel of a theme
