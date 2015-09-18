fbd3058dae5f82382a61b915cff3700fbf82086c
Will <will.doran@riseup.net>
2015-09-17 23:23:19 -0400

    testing theme with override csse

--- source/conf.py
+++ source/conf.py
@@ -156,7 +156,8 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
+def setup(app):
+    app.add_stylesheet("theme_overrides.css")
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
 # directly to the root of the documentation.
