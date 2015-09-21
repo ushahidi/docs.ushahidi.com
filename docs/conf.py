4f44f21f2948d4f1ece5eb374e4641d0263d78bd
Will <will.doran@riseup.net>
2015-09-21 18:19:37 -0400

    still testing

--- docs/conf.py
+++ docs/conf.py
@@ -163,7 +163,7 @@ html_theme_path = ['_theme']
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_static_path = ['_static']
 #def setup(app):
 #    app.add_stylesheet("theme_overrides.css")
 # Add any extra paths that contain custom files (such as robots.txt or
